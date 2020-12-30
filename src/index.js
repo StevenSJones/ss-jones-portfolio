1c67810513d12af0e1b2bdb3b2e578606be34a93
SS Jones <SSJones.code@gmail.com>
2020-12-29 19:05:00 -0700

    I began changing the function component to a class component with comments.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-//below is a "ticking clock" that starts when the page is loaded and tells the user the time, locally.
+/* You can convert a function component like Clock to a class in five steps:
+1 Create an ES6 class, with the same name, that extends React.Component.
+2 Add a single empty method to it called render().
+3 Move the body of the function into the render() method.
+4 Replace props with this.props in the render() body.
+5 Delete the remaining empty function declaration.*/
+
+//clock class component definition:
+// class Clock extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Welcome</h1>
+//         <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+
+//below is a "ticking clock" function that starts when the page is loaded and tells the user the time locally
 function tick() {
   const element = (
     <div>
@@ -13,7 +32,7 @@ function tick() {
     </div>
   );
   // highlight-next-line
-  ReactDOM.render(element, document.getElementById('root'));
+  ReactDOM.render(element, document.getElementById("root"));
 }
 
 setInterval(tick, 1000);
