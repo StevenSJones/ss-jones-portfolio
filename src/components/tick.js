d76fc01a1d8389f68e47de6ef9e7cac4a3353209
SS Jones <SSJones.code@gmail.com>
2020-12-30 15:06:10 -0700

    I hit a dead end with the clock component saying that the element was not declared so I pivoted and started further back and created a single function that just prints Hello React to the browser page. The clock was added to the todos in the readme.md file.

---
+++ src/components/tick.js
@@ -0,0 +1,26 @@
+// import React from "react";
+
+
+
+
+
+
+
+
+
+
+
+
+// below is a "ticking clock" function that starts when the page is loaded and tells the user the time locally
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Welcome</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+  
+//   setInterval(tick, 1000);
+
+//   export default tick();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-// import App from './App';
-import reportWebVitals from "./reportWebVitals";
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
 
 /* You can convert a function component like Clock to a class in five steps:
 1 Create an ES6 class, with the same name, that extends React.Component.
@@ -11,7 +12,7 @@ import reportWebVitals from "./reportWebVitals";
 4 Replace props with this.props in the render() body.
 5 Delete the remaining empty function declaration.*/
 
-//clock class component definition:
+// clock class component:
 // class Clock extends React.Component {
 //   render() {
 //     return (
@@ -23,21 +24,8 @@ import reportWebVitals from "./reportWebVitals";
 //   }
 // }
 
-//below is a "ticking clock" function that starts when the page is loaded and tells the user the time locally
-function tick() {
-  const element = (
-    <div>
-      <h1>Welcome</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  // highlight-next-line
-  ReactDOM.render(element, document.getElementById("root"));
-}
 
-setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
