bc8de22bcc0f7de7460ebd97c0dc776effa9b928
SS Jones <SSJones.code@gmail.com>
2021-01-02 13:22:08 -0700

    I removed images that were not needed and I included the images that I may use in the newly created img dir inside of public.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from "react";
 import HelloReact from "./components/HelloReact";
 import Enjoy from "./components/IEnjoy";
 import Navigation from "./components/Navigation";
-
-
-
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navigation />
-      <HelloReact name="Steven"/> 
+      <About />
+      {/* <HelloReact name="Steven"/> 
       <Enjoy fav1="hockey" fav2="skateboarding" fav3="drawing" />
       <HelloReact name="GeOvonni"/> 
       <Enjoy fav1="gaming" fav2="chillin" fav3="girls" />
       <HelloReact name="Shayla"/> 
-      <Enjoy fav1="biking" fav2="binging" fav3="reading" />
+      <Enjoy fav1="biking" fav2="binging" fav3="reading" /> */}
     </>
   );
 }
