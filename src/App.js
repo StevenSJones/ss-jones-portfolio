6222783d1e872dbeed3295ef0d85a0227557e38d
SS Jones <SSJones.code@gmail.com>
2021-01-17 13:54:00 -0700

    I removed the HelloReact.js and IEnjoy.js files from the components dir as well as the imports for the aforementioned components in the App.js file and the subsequent els that represent them in the App function in the same file as they were for practice.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-// import HelloReact from "./components/HelloReact";
-// import Enjoy from "./components/IEnjoy";
 import About from "./components/About";
 import NavigationHeader from "./components/NavigationHeader";
 import Portfolio from "./components/Portfolio";
@@ -14,12 +12,6 @@ function App() {
       <About />
       <Portfolio />
       <Contact />
-      {/* <HelloReact name="Steven"/> 
-      <Enjoy fav1="hockey" fav2="skateboarding" fav3="drawing" />
-      <HelloReact name="GeOvonni"/> 
-      <Enjoy fav1="gaming" fav2="chillin" fav3="girls" />
-      <HelloReact name="Shayla"/> 
-      <Enjoy fav1="biking" fav2="binging" fav3="reading" /> */}
     </>
   );
 }
