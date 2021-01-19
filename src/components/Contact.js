c04f8e501290e4a8d5311c3714d2a8e379ae4243
SS Jones <SSJones.code@gmail.com>
2021-01-18 18:56:24 -0700

    I amended the styling for the img and added a class so that the img was sizes better.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Contact(props) {
-  
   return (
     <>
       <div id="contact" className="text-center">
@@ -11,39 +10,46 @@ export default function Contact(props) {
             <hr />
           </div>
 
-          <div className="col text-center">
-            <img src="img/contact/contact.jpg" className="img-responsive contact-img" alt="Steven Jones in a houndstooth jacket and black shirt." />
+          <div className="row text-center">
+            <div className="col-12 img-wrapper">
+              <img
+                src="img/contact/contact.jpg"
+                className="contact-img"
+                alt="Steven Jones in a houndstooth jacket and black shirt."
+              />
+              <p className="emailContact">
+                <a
+                  className="emailContact"
+                  href="mailto:ssjones.code@gmail.com?Subject=KHA%20Portfolio"
+                  target="_top"
+                >
+                  Send Me An Email
+                </a>
+              </p>
+            </div>
           </div>
 
-          <div className="col">
-            <p className="emailContact">
-              <a
-                className="emailContact"
-                href="mailto:ssjones.code@gmail.com?Subject=KHA%20Portfolio"
-                target="_top"
-              >
-                Send Me An Email
-              </a>
-            </p>
-
-            <div className="social">
-              <ul>
-                <li>
-                  <a href="https://github.com/StevenSJones">
-                    <i className="fa fa-github fa-4x"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/in/steven-j-285a33115/">
-                    <i className="fa fa-linkedin fa-4x"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://twitter.com/ss_jones">
-                    <i className="fa fa-twitter fa-4x"></i>
-                  </a>
-                </li>
-              </ul>
+          <div className="row text-center">
+            <div className="col-12">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href="https://github.com/StevenSJones">
+                      <i className="fa fa-github fa-4x"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/steven-j-285a33115/">
+                      <i className="fa fa-linkedin fa-4x"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/ss_jones">
+                      <i className="fa fa-twitter fa-4x"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -51,3 +57,7 @@ export default function Contact(props) {
     </>
   );
 }
+
+//importing icons from iconify (https://iconify.design/icon-sets/?query=twitter) then i ran yarn add --save @iconify/react @iconify-icons/fa-brands
+// import { twitterIcon } from '@iconify/react';
+// import twitterIcon from '@iconify-icons/fa-brands/twitter';
